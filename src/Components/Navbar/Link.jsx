35c8b727936a8f514d048ef8660383ade203fd7a
AhmedSami019 <easami019192@gmail.com>
2025-10-17 21:22:30 +0600

    nake responsive navbar

--- src/Components/Navbar/Link.jsx
+++ src/Components/Navbar/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({item}) => {
     return (
-        <li className='mr-10 text-lg'>
+        <li className='mr-5 text-lg px-5   bg-black md:bg-transparent rounded text-center '>
             <a href={item.path}>{item.name}</a>
         </li>
     );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from 'lucide-react';
 const routes = [
-  { id: 1, name: 'Home', path: '/' },
-  { id: 2, name: 'About', path: '/about' },
-  { id: 3, name: 'Services', path: '/services' },
-  { id: 4, name: 'Contact', path: '/contact' },
-  { id: 5, name: 'Blog', path: '/blog' },
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Services", path: "/services" },
+  { id: 4, name: "Contact", path: "/contact" },
+  { id: 5, name: "Blog", path: "/blog" },
 ];
 
-
 const Navbar = () => {
-    return (
-        <div className='flex justify-between text-lg mx-8 py-6'>
-            <h1>MyNav</h1>
-            <ul className='flex'>
-                {
-                    routes.map(item => <Link item={item}></Link>)
-                }
-            </ul>
-            <button>Sign in</button>
-        </div>
-    );
+const [open, setOpen] = useState()
+
+const link = routes.map((item) => (
+          <Link item={item}></Link>
+        ))
+
+  return (
+    <div className="flex justify-between text-lg mx-8 py-6">
+      <div className="flex " onClick={()=> setOpen(!open)}>
+        <ul className={`md:hidden absolute duration-100 ${open ? "top-12": "-top-40"} divide-y-2 divide-gray-600`}>
+          {
+            link
+          }
+        </ul>
+        {
+          open ? <X className="md:hidden"></X> :  <Menu className="md:hidden" />
+        }
+        <h1 className="ml-5">MyNav</h1>
+      </div>
+      <ul className="hidden md:flex ">
+        {
+            link
+        }
+      </ul>
+      <button>Sign in</button>
+    </div>
+  );
 };
 
 export default Navbar;
